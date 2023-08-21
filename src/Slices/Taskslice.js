fc27b16a16df98eb67807d8e9d82c1c8469880c3
jinen <108358045+jinen26@users.noreply.github.com>
2023-08-21 12:57:53 +0200

    Update Taskslice.js

--- src/Slices/Taskslice.js
+++ src/Slices/Taskslice.js
@@ -1,37 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-// const initialState = {
-//     tasks:[
-//         {
-//     name:"LearnHTML",
-//     isDone:false,
-//         },
-//         {
-//     name:"LearnCSS",
-//     isDone:true,
-//          },
-//         {
-//     name:"LearnJS",
-//     isDone:false,
-//         }
-//     ]
-// }
 
-// export const TaskSlice = createSlice({
-//   name: 'todo',
-//   initialState,
-//   reducers: {
-//     addtask:(state,action)=>{
-//         state.tasks=[...state.tasks,action.payload]
-//     }
-
-// }})
-
-// // Action creators are generated for each case reducer function
-// export const { addtask} = TaskSlice.actions
-
-// export default TaskSlice.reducer
-
-import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   tasks:[
@@ -55,7 +23,8 @@ export const TaskSlice = createSlice({
   initialState,
   reducers: {
     addtask:(state,action)=>{
-        state.task=[...state.tasks,action.payload]
+        state.tasks=[...state.tasks,action.payload]
+        
     }
   },
 });
