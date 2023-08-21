552a2aafb5bcba247434388ef956428e5abbea75
jinen <108358045+jinen26@users.noreply.github.com>
2023-08-21 12:57:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './store'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import {store} from './store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  
+<Provider store={store}>
   <App/>
 </Provider>
-    
+
 );
 
 // If you want to start measuring performance in your app, pass a function
